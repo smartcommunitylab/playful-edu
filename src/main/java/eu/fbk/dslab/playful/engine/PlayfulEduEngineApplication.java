2effac7322b553eacf8c5bc7dafb76964ec12b8a
micnori <micnori@users.noreply.github.com>
2023-06-16 10:26:43 +0200

    first import

---
+++ src/main/java/eu/fbk/dslab/playful/engine/PlayfulEduEngineApplication.java
@@ -0,0 +1,13 @@
+package eu.fbk.dslab.playful.engine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlayfulEduEngineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlayfulEduEngineApplication.class, args);
+	}
+
+}
---
+++ src/test/java/eu/fbk/dslab/playful/engine/PlayfulEduEngineApplicationTests.java
@@ -0,0 +1,13 @@
+package eu.fbk.dslab.playful.engine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlayfulEduEngineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
